934ae85ad6ac8b0787dd9cf39892149d625f999f
Maxtasy <maxtasy88@web.de>
2020-12-01 14:11:06 +0100

    Add day 1 solution

---
+++ day01-1.py
@@ -0,0 +1,18 @@
+def part1(input_file):
+  with open(input_file, "r") as f:
+    input_str = f.read().strip()
+    number_array = list(map(int, input_str.split("\n")))
+    for i in range(len(number_array)):
+      for j in range(len(number_array) - 1 - i):
+        result = number_array[i] + number_array[i+j+1]
+        if result == 2020:
+          return number_array[i] * number_array[i+j+1]
+
+
+def main():
+  input_file = "day01-input.txt"
+  print(part1(input_file))
+
+
+if __name__ == "__main__":
+  main()
---
+++ day01-2.py
@@ -0,0 +1,19 @@
+def part2(input_file):
+  with open(input_file, "r") as f:
+    input_str = f.read().strip()
+    number_array = list(map(int, input_str.split("\n")))
+    for i in range(len(number_array)):
+      for j in range(len(number_array) - 1 - i):
+        for k in range(len(number_array) - 1 - i - j):
+          result = number_array[i] + number_array[i+j+1] + number_array[i+k+1]
+          if result == 2020:
+            return number_array[i] * number_array[i+j+1] * number_array[i+k+1]
+
+
+def main():
+  input_file = "day01-input.txt"
+  print(part2(input_file))
+
+
+if __name__ == "__main__":
+  main()
